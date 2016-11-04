6e76b33bbe97e21705e0548126952c8d1c483ed1
ea <elliea915@gmail.com>
2016-11-04 15:31:05 -0700

    now have my best guess at showing the price

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,31 @@
-// function Pizza(size, toppings) {
-//   this.sizes = size;
-//   this.toppings = toppings;
-//   this.price= 0;
-// }
-//
-// var cutomerOrder;
-//
-// Pizza.prototype.orderTotal = function() {
-//   alert("hello")
-//   this.price =0;
-//   if (this.size === "Small"){
-//    this.price += 14;
-//  } else if (this.size === "Medium") {
-//    this.price +=16;
-//  } else if (this.size === "Large") {
-//    this.price += 18;
-//  } for (var i = 0; i < this.toppings.length; i++) {
-//    this.price += 0.5;
-//  } return this.price.toFixed(2);
-// }
+function Pizza(size, toppings) {
+  this.sizes = size;
+  this.toppings = toppings;
+  this.price= 0;
+}
+
+var cutomerOrder;
+
+Pizza.prototype.orderTotal = function() {
+  alert("hello")
+  this.price =0;
+  if (this.size === "Small"){
+   this.price += 14;
+ } else if (this.size === "Medium") {
+   this.price +=16;
+ } else if (this.size === "Large") {
+   this.price += 18;
+ } for (var i = 0; i < this.toppings.length; i++) {
+   this.price += 0.5;
+ } return this.price.toFixed(2);
+}
 
 
 
 //userInterface
 $(document).ready(function(){
+
+  //  var newPizza = new Pizza()
 
   $("form.order").submit(function(event){
     event.preventDefault();
@@ -39,6 +41,11 @@ $(document).ready(function(){
        var toppings = $(this).val();
        customerToppings.push(toppings);
      });
+
+     $("#.total").show();
+     var newPizza = new Pizza(size,toppings);
+     $("#total").text(newPizza)
+
    });
   });
 
