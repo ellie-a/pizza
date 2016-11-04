ddb63026e64291ddb6963725f81fe68884eb4f9a
ea <elliea915@gmail.com>
2016-11-04 14:44:29 -0700

    update right val's read in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,48 @@
-function Pizza(size, toppings,price) {
-  this.sizes = size;
-  this.toppings = toppings;
-  this.price= 0;
-}
-var cutomerOrder;
-
-
-Pizza.prototype.orderTotal = function() {
-  if (this.size === "Small"){
-   this.price += 14;
- } else if (this.size === "Medium") {
-   this.price +=16;
- } else if (this.size === "Large") {
-   this.price += 18;
- } for (var i = 0; i < this.toppings.length; i++) {
-   this.price += 0.5;
- } return this.price.toFixed(2);
-}
-
-
-
-// adding for loops  size price and for topping price
-// add all
-// give it to customer
-// call on customer in front end
-
-
-
-
-
-
-
-
+// function Pizza(size, toppings) {
+//   this.sizes = size;
+//   this.toppings = toppings;
+//   this.price= 0;
+// }
+//
+// var cutomerOrder;
+//
+// Pizza.prototype.orderTotal = function() {
+//   alert("hello")
+//   this.price =0;
+//   if (this.size === "Small"){
+//    this.price += 14;
+//  } else if (this.size === "Medium") {
+//    this.price +=16;
+//  } else if (this.size === "Large") {
+//    this.price += 18;
+//  } for (var i = 0; i < this.toppings.length; i++) {
+//    this.price += 0.5;
+//  } return this.price.toFixed(2);
+// }
 
 
 
 //userInterface
 $(document).ready(function(){
+
   $("form.order").submit(function(event){
     event.preventDefault();
+    debugger;
+
     $("#pizzaSize").show();
-    $("input:radio[name=size]:checked").each(function(){
-      var pizzaSize = $(this).val();
-      $("#pizzaSize").append(pizzaSize + "<br>");
-    });
+    var size = $("input:radio[name=size]:checked").val();
+
+
     $("#pizzaToppings").show();
-    $("input:checkbox[name=toppings]:checked").each(function(){
-      var pizzaToppings = $(this).val();
-      $("#pizzaToppings").append(pizzaToppings + "<br>");
-    });
+    var customerToppings = [];
+   $("input:checkbox[name=toppings]:checked").each(function() {
+       var toppings = $(this).val();
+       customerToppings.push(toppings);
+     });
+   });
   });
-});
+
+
+    // $("#price").show();
+    // var yourPrice = customerOrder.orderTotal();
+    // $("#price").append(yourPrice + "<br>");
